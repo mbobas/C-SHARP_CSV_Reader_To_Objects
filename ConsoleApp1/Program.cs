4fb3a8111ca1e9a1f9326b02e518702c6ca52b29
Michal <mbobas@gmail.com>
2021-03-09 19:57:20 +0100

    ostateczna fukcja nr 2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -66,12 +66,11 @@ namespace ConsoleApp1
             }
             return result;
         }
-
-        public List<DzienPracy> ReadFileCompany22()
+        public List<DzienPracy> ReadFileCompany2()
         {
             List<DzienPracy> result = new List<DzienPracy>();
             StreamReader dayReader = new StreamReader("rcp2.csv");
-        
+
             bool readOldLine = false;
             var line = "";
 
@@ -83,11 +82,12 @@ namespace ConsoleApp1
                     if (readOldLine)
                     {
                         readOldLine = false;
-                    }else
-                    {
-                         line = dayReader.ReadLine();
                     }
-                    
+                    else
+                    {
+                        line = dayReader.ReadLine();
+                    }
+
                     if (line_number >= 0)
                     {
                         var values = line.Split(';');
@@ -99,12 +99,12 @@ namespace ConsoleApp1
                             day.Data = Convert.ToDateTime(values[1]);
                             try
                             {
-                                if (values[3] == "WE" && values[2] != null)
+                                if (values[3] == "WE" /*&&values[2] != null*/)
                                 {
                                     day.GodzinaWejscia = TimeSpan.Parse(values[2] + ":00");
                                     var line2 = dayReader.ReadLine();
                                     var values2 = line2.Split(';');
-                                    if (values2[0] == values[0] && values2[1] == values[1] && values2[2] != null)//+data value1
+                                    if (values2[0] == values[0] && values2[1] == values[1] && !values2[2].Equals(""))
                                     {
                                         day.GodzinaWyjscia = TimeSpan.Parse(values2[2] + ":00");
                                     }
@@ -115,12 +115,12 @@ namespace ConsoleApp1
                                         continue;
                                     }
                                 }
-                                else if (values[3] == "WY" && values[2] != null)
+                                else if (values[3] == "WY" /*&&values[2] != null*/)
                                 {
                                     day.GodzinaWyjscia = TimeSpan.Parse(values[2] + ":00");
                                     var line2 = dayReader.ReadLine();
                                     var values2 = line2.Split(';');
-                                    if (values2[0] == values[0] && values2[1] == values[1] && values2[2] != null)//+data value1
+                                    if (values2[0] == values[0] && values2[1] == values[1] && !values2[2].Equals(""))
                                     {
                                         day.GodzinaWejscia = TimeSpan.Parse(values2[2] + ":00");
                                     }
@@ -150,99 +150,18 @@ namespace ConsoleApp1
             }
             return result;
         }
-
-        public List<DzienPracy> ReadFileCompany2()
-        {
-            List<DzienPracy> result = new List<DzienPracy>();
-            StreamReader dayReader = new StreamReader("rcp2.csv");
-
-            bool readOldLine = false;
-            var line = "";
-
-            if (dayReader != null)
-            {
-                int line_number = 0;
-                while (!dayReader.EndOfStream)
-                {
-                    if (readOldLine)
-                    {
-                        readOldLine = false;
-                    }
-                    else
-                    {
-                        line = dayReader.ReadLine();
-                    }
-
-                    if (line_number >= 0)
-                    {
-                        var values = line.Split(';');
-
-                        DzienPracy day = new DzienPracy();
-                        day.KodPracownika = Convert.ToString(values[0]);
-                        day.Data = Convert.ToDateTime(values[1]);
-                        try
-                        {
-                            if (values[3] == "WE" && values[2] != null)
-                            {
-                                day.GodzinaWejscia = TimeSpan.Parse(values[2] + ":00");
-                                var line2 = dayReader.ReadLine();
-                                var values2 = line2.Split(';');
-                                if (values2[0] == values[0] && values2[1] == values[1] && values2[2] != null)
-                                {
-                                    day.GodzinaWyjscia = TimeSpan.Parse(values2[2] + ":00");
-                                }
-                                else
-                                {
-                                    line = line2;
-                                    readOldLine = true;
-                                    continue;
-                                }
-                            }
-                            else if (values[3] == "WY" && values[2] != null)
-                            {
-                                day.GodzinaWyjscia = TimeSpan.Parse(values[2] + ":00");
-                                var line2 = dayReader.ReadLine();
-                                var values2 = line2.Split(';');
-                                if (values2[0] == values[0] && values2[1] == values[1] && values2[2] != null)//+data value1
-                                {
-                                    day.GodzinaWejscia = TimeSpan.Parse(values2[2] + ":00");
-                                }
-                                else
-                                {
-                                    line = line2;
-                                    readOldLine = true;
-                                    continue;
-                                }
-                            }
-                            else if (values[2] == null)
-                            {
-                                continue;
-                            }
-                        }
-                        catch (System.FormatException ex)
-                        {
-                            continue;
-                        }
-                        result.Add(day);
-
-                    }
-                    line_number++;
-                }
-                dayReader.Close();
-            }
-            return result;
-        }
+        
         public void menu()
         {
             Program program = new Program();
             List<DzienPracy> dzien1 = program.ReadFileCompany1();
-            List<DzienPracy> dzien2 = program.ReadFileCompany22();
+            List<DzienPracy> dzien2 = program.ReadFileCompany2();
             
             Console.WriteLine("***********MENU***********\n");
             Console.WriteLine("***Nacisnij odpowiedni klawisz***\n");
             Console.WriteLine("Program Wczytuje 2 pliki o nazwach  rcp1.csv i rcp2.csv \n Dzieje sie to automatycznie z folderu ConsoleApp1\\bin\\Debug\\ \nczyli tego, w ktorym znajduje sie aplikacja \n\n");
-            Console.WriteLine("Dla pliku rcp1 program uwzględnia wyjątki braku danych w komórkach, mimo, że przykładowy plik był kompletny");
-            Console.WriteLine("Dla pliku rcp2 program pomija wiersz jeśli na dany dzien przypadło jedno wejście lub wyjscie ");
+            Console.WriteLine("Dla pliku rcp1 program uwzględnia wyjątki braku danych w różnych komórkach, mimo, że przykładowy plik był kompletny\n");
+            Console.WriteLine("Dla pliku rcp2 program pomija wiersz jeśli na dany dzien przypadło jedno wejście lub wyjscie, robi to również w przypadku braku innych danych wejściowych. ");
 
             Console.WriteLine("Wciśnij (1), aby wyświetlic wczytaną listę dla pliku rcp1.csv\n");
             Console.WriteLine("Wciśnij (2), aby wyświetlic wczytaną listę dla pliku rcp2.csv\n");
